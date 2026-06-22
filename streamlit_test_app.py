223bb604d226a7f8b79ba7c5479db9fcd646be26
mudasir1134 <razamudasir1134@gmail.com>
2026-06-22 23:11:43 +0500

    Resolved error in running interface

--- streamlit_test_app.py
+++ streamlit_test_app.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import pickle
 
-with open('log_model.pkl', 'rb') as file:
+with open('models/log_model.pkl', 'rb') as file:
     log_model = pickle.load(file)
 
 def predict_customer_churn(
